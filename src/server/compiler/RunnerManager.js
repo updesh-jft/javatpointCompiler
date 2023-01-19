3a5bfa179ca3a334584ea5201f68e4bccedb50a3
updesh-jft <updesh-jft>
2023-01-19 11:31:44 +0530

    iframe port updated

--- src/server/compiler/RunnerManager.js
+++ src/server/compiler/RunnerManager.js
@@ -78,7 +78,7 @@ module.exports = {
         url,
         fileName
       };
-      result.url = `http://localhost:8080/${fileNamee}.html`;
+      result.url = `http://localhost:8888/${fileNamee}.html`;
       result.fileName = `${fileNamee}.html`;
       res.end(JSON.stringify(result));
     } else {
